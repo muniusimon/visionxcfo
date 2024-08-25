768ef5a0307b0da2fc938d99cba49cc3947e3b01
muniusimon <muniusimon@gmail.com>
2024-08-25 03:09:15 +0300

    Added contact route

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ def home():
 @app.route('/about')
 def about ():
     return "<h1>This is our about us</h1>"
+
+@app.route ('/contact')
+def contact ():
+    return "<h1> You are welcome. Please contact us</h1>"
